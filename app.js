0e4f6235714619639dfa9bc559b256ce2d517aa3
MalbanNGI <127600146+MalbanNGI@users.noreply.github.com>
2024-10-14 14:24:29 +0200

    modifs importantes sur la database

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const userModel = require("./models/user.js");
+
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.render('index');
+// app.get('/', function (req, res) { // exemple     async si await
+   //  res.render('index');
+// })
+
+app.get('/', async function (req, res) { // {param}
+    try {
+        const user = await userModel.getUserById(2); // chiffre a recup normalement
+        res.render('testing', {user});
+        console.log(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Erreur lors de la recu des données !');
+    }
 })
 
 
