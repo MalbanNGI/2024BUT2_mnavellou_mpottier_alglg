e4a2c6572d9395aeae99a35085e3a16004bab531
MalbanNGI <127600146+MalbanNGI@users.noreply.github.com>
2024-10-15 11:31:20 +0200

    frez
    zeaze

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // gere les root //
 
 const express = require("express");
-// const session = require("express-session");
+const session = require("express-session");
+const md5 = require("md5");
 const app = express();
 const userModel = require("./models/user.js");
 
@@ -9,34 +10,21 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-// app.use((req, res, next) => {
-//   if (req.session && req.session.login) {
-//     res.locals.isLoggedIn = true;
-//     res.locals.login = req.session.login;
-//     res.locals.role = req.session.role;
-//     res.locals.nom = req.session.nom;
-//   } else {
-//     res.locals.isLoggedIn = false;
-//   }
-//   next();
-// });
+app.use(express.urlencoded({extended: false}));
 
-// app.use(
-//   session({
-//     secret: "MILO",
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false }, // À true si vous avez un HTTPS
-//   })
-// );
-// // test session 
-// app.get('/test-session', (req, res) => {
-//     res.send(res.locals);
-//   });
+app.use(session({
+  secret: 'milo',
+  resave: false,
+  saveUninitialized: false
+}))
+
 
 // Liens vers les différentes pages
 
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
+  if(!req.session.userId) { // le ! veut dire : req.session.userId == false
+    return res.redirect("/connexion"); // verif si l'accès est okay !!!!!! 
+  }
   // exemple     async si await
   res.render("index");
 });
@@ -56,8 +44,9 @@ app.get("/contact", function (req, res) {
 app.get("/inscription", function (req, res) {
   res.render("inscription");
 });
-app.get("/connexion", function (req, res) {
-  res.render("connexion");
+
+app.get("/produit", function (req, res) { // chemin vers la page produit
+  res.render("produit");
 });
 
 app.get("/test", async function (req, res) {
@@ -72,46 +61,28 @@ app.get("/test", async function (req, res) {
   }
 });
 
-// Données de session
-app.get("/set-session/:name", async function (req, res) {
-  // Correction du paramètre
-  try {
-    // Récupération de l'utilisateur par nom depuis la variable name
-    const user = await userModel.getUserByName(req.params.name);
 
-    if (user && user.length > 0) {
-      // Vérifie si un utilisateur a été trouvé
-      const userData = user[0]; // Supposons qu'il n'y ait qu'un utilisateur dans le tableau
-      req.session.login = userData.login;
-      req.session.role = userData.type_utilisateur;
-      req.session.nom = userData.nom;
-
-      // Rendre la vue 'index' avec les données utilisateur
-      res.render("index", { user: userData });
-      console.log(userData);
-    } else {
-      res.status(404).send("Utilisateur non trouvé");
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Erreur lors de la récupération des données !");
+// connexion utilisateurs 
+app.get("/connexion", function (req, res) {
+  res.render("connexion", {error: null});
+});
+app.post("/connexion", async function (req, res) {
+  let login = req.body.email;
+  let mdp = req.body.password;
+  mdp = md5(mdp);
+  const user = await userModel.check_login(login);
+  if(user != false && user.password == mdp) { 
+    req.session.userId = user.id;  // Récupère les données
+    req.session.role = user.type_utilisateur;
+    return res.redirect("/");
+  }
+  else {
+    res.render("login", {error: "Mauvais login/mdp"});
   }
 });
 
-// Route pour récupérer la variable de session
-app.get("/get-session", (req, res) => {
-  if (req.session.login && req.session.role && req.session.nom) {
-    // Renvoyer un objet avec toutes les informations de session
-    res.json({
-      login: req.session.login,
-      role: req.session.role,
-      nom: req.session.nom,
-    });
-  } else {
-    res.send("Aucune session définie.");
-  }
-});
 
+// 
 app.use(function (req, res) {
   res.status(404).render("404");
 });
